0f2bdfba118fa2e4cb1fb5f4f359f6e87f290abc
HumbertoDiego <humberto-xingu@live.com>
2022-02-28 16:08:45 -0400

    Adicionado gitignore

--- se6alunos/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import dp, successcount
-
-admin.site.register(dp)
-admin.site.register(successcount)
--- se6alunos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Se6AlunosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'se6alunos'
