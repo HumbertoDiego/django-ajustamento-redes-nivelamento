382d566478defb5e767fcd2dd0e2f70d0cbb156c
HumbertoDiego <humberto-xingu@live.com>
2022-03-02 21:13:53 -0400

    Adicionado proteção contra entrada arquivos texto fora do padrão

--- ajunivel/urls.py
+++ ajunivel/urls.py
@@ -2,10 +2,10 @@
 from django.urls import path,re_path
 from . import views
 
+app_name = 'ajunivel'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index', views.index, name='index'),
     path('index.html', views.index, name='index'),
-    path('ajunivel', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('menu', views.menu, name='menu'),
 ]
--- ajunivel/views.py
+++ ajunivel/views.py
@@ -22,59 +22,63 @@ def index(request):
         # create a form instance and populate it with data from the request:
         form = AjustamentoCustomForm(request.POST, request.FILES)
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            X = form.cleaned_data['dp']
-            auto = form.cleaned_data['automatico']
-            dp_na_4col = form.cleaned_data['dpinformado']
-            c = []
-            for line in request.FILES['arquivo']:
-                if len(line.split()) == 4:
-                    c.append([l.decode("utf-8")  for l in line.split()])
-                else: # acrescentar 0 na 1º pos das linhas que só possuírem 3 dados e gravar tudo em uma matriz c[] para aceitar injunções
-                    temp = [l.decode("utf-8")  for l in line.split()]
-                    temp.insert(0, 0)
-                    c.append(temp)
-            retorno = NivParamet.parametrico(c, X, auto, dp_na_4col).copy()
-            # atualizar o succesos count
-            if len(successcount.objects.all()):
-                old  = successcount.objects.all()[0].contador
-                new = successcount.objects.filter(pk=1)
-                new.update(contador=str(int(old)+1))
-            else:
-                successcount.objects.create(contador="0")
-            tab1 = ""
-            for i in range(0,len(c)):
-                tab1 += "<tr>"
-                for k in range(0,len(c[i])):
-                    tab1 += "<td>"+c[i][k]+"</td>"
-                tab1 += "<td>"+str(retorno['dp'][i])+"</td>"
-                tab1 += "<td>"+str(retorno['La'][i])+"</td>"
-                tab1 += "<td>"+str(retorno['V'][i])+"</td>"
-                tab1 += "</tr>"
-            retorno['tab1'] = tab1
-            tab2 = ""
-            for i in range(0,len(retorno['Incognitas'])):
-                tab2 += "<tr>"
-                tab2 += "<td>"+str(retorno['Incognitas'][i])+"</td>"
-                tab2 += "<td>"+str(retorno['X'][i])+"</td>"
-                tab2 += "<td>"+str(retorno['desvio_hi'][i])+"</td>"
-                tab2 += "</tr>"
-            retorno['tab2'] = tab2
-            tab3 = ""
-            for i in range(0,len(c)):
-                tab3 += "<tr>"
-                tab3 += "<td>"+str(i+1)+"</td>"
-                for k in range(0,len(c[i])):
-                    tab3 += "<td>"+c[i][k]+"</td>"
-                tab3 += "<td>"+str(retorno['V'][i])+"</td>"
-                tab3 += "<td>"+str(retorno['desvio_vi'][i])+"</td>"
-                tab3 += "<td>"+str(retorno['wi'][i])+"</td>"
-                if retorno['aceitar']=='aceito':
-                    tab3 += "<td style='color:green'>Não existe evidências estatísticas para a rejeição</td>"
+            try:
+                # process the data in form.cleaned_data as required
+                X = form.cleaned_data['dp']
+                auto = form.cleaned_data['automatico']
+                dp_na_4col = form.cleaned_data['dpinformado']
+                c = []
+                for line in request.FILES['arquivo']:
+                    if len(line.split()) == 4:
+                        c.append([l.decode("utf-8")  for l in line.split()])
+                    else: # acrescentar 0 na 1º pos das linhas que só possuírem 3 dados e gravar tudo em uma matriz c[] para aceitar injunções
+                        temp = [l.decode("utf-8")  for l in line.split()]
+                        temp.insert(0, 0)
+                        c.append(temp)
+                retorno = NivParamet.parametrico(c, X, auto, dp_na_4col).copy()
+                # atualizar o succesos count
+                if len(successcount.objects.all()):
+                    old  = successcount.objects.all()[0].contador
+                    new = successcount.objects.filter(pk=1)
+                    new.update(contador=str(int(old)+1))
                 else:
-                    tab3 += "<td style='color:red'>Existe evidências estatísticas para a rejeição</td>"
-                tab3 += "</tr>"
-            retorno['tab3'] = tab3
+                    successcount.objects.create(contador="0")
+                tab1 = ""
+                for i in range(0,len(c)):
+                    tab1 += "<tr>"
+                    for k in range(0,len(c[i])):
+                        tab1 += "<td>"+c[i][k]+"</td>"
+                    tab1 += "<td>"+str(retorno['dp'][i])+"</td>"
+                    tab1 += "<td>"+str(retorno['La'][i])+"</td>"
+                    tab1 += "<td>"+str(retorno['V'][i])+"</td>"
+                    tab1 += "</tr>"
+                retorno['tab1'] = tab1
+                tab2 = ""
+                for i in range(0,len(retorno['Incognitas'])):
+                    tab2 += "<tr>"
+                    tab2 += "<td>"+str(retorno['Incognitas'][i])+"</td>"
+                    tab2 += "<td>"+str(retorno['X'][i])+"</td>"
+                    tab2 += "<td>"+str(retorno['desvio_hi'][i])+"</td>"
+                    tab2 += "</tr>"
+                retorno['tab2'] = tab2
+                tab3 = ""
+                for i in range(0,len(c)):
+                    tab3 += "<tr>"
+                    tab3 += "<td>"+str(i+1)+"</td>"
+                    for k in range(0,len(c[i])):
+                        tab3 += "<td>"+c[i][k]+"</td>"
+                    tab3 += "<td>"+str(retorno['V'][i])+"</td>"
+                    tab3 += "<td>"+str(retorno['desvio_vi'][i])+"</td>"
+                    tab3 += "<td>"+str(retorno['wi'][i])+"</td>"
+                    if retorno['aceitar']=='aceito':
+                        tab3 += "<td style='color:green'>Não existe evidências estatísticas para a rejeição</td>"
+                    else:
+                        tab3 += "<td style='color:red'>Existe evidências estatísticas para a rejeição</td>"
+                    tab3 += "</tr>"
+                retorno['tab3'] = tab3
+            except:
+                retorno = True
+                erro = True
     else:
         form = AjustamentoCustomForm()
     context = {
